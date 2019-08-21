9fb9c769f09b89331c2ce43eed40d8188886c93f
BILLthebuilder <billkariri@gmail.com>
2019-08-21 22:22:02 +0300

    An intro to react using a barebones config(helps me understand how react works)

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+    return React.createElement(
+        'div',
+        {},
+        [
+            React.createElement('h1', {}, name),
+            React.createElement('h2', {}, animal),
+            React.createElement('h2', {}, breed)
+        ]
+    );
+}
+
+const App = () => {
+    return React.createElement(
+        'div',
+        {},
+        [
+            React.createElement('h1', {}, 'Adopt me!'),
+            React.createElement(Pet, { name: 'garfield', animal: 'cat', breed: 'fat' }),
+            React.createElement(Pet, { name: 'mbuku', animal: 'rabbit', breed: 'white' }),
+            React.createElement(Pet, { name: 'Babu', animal: 'dog', breed: 'chihuahua' })
+
+        ]
+    );
+}
+
+ReactDOM.render(
+    React.createElement(App, Pet),
+    document.getElementById('root')
+);

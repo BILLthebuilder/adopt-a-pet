5a263af3ebb1e691a4a8360038617306e59a1bf0
BILLthebuilder <billkariri@gmail.com>
2019-08-22 22:10:08 +0300

    add basic tooling for linting and code quality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement(
-        'div',
-        {},
-        [
-            React.createElement('h1', {}, name),
-            React.createElement('h2', {}, animal),
-            React.createElement('h2', {}, breed)
-        ]
-    );
-}
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, name),
+        React.createElement('h2', {}, animal),
+        React.createElement('h2', {}, breed)
+    ]);
+};
 
 const App = () => {
-    return React.createElement(
-        'div',
-        {},
-        [
-            React.createElement('h1', {}, 'Adopt me!'),
-            React.createElement(Pet, { name: 'garfield', animal: 'cat', breed: 'fat' }),
-            React.createElement(Pet, { name: 'mbuku', animal: 'rabbit', breed: 'white' }),
-            React.createElement(Pet, { name: 'Babu', animal: 'dog', breed: 'chihuahua' })
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'Adopt me!'),
+        React.createElement(Pet, { name: 'garfield', animal: 'cat', breed: 'fat' }),
+        React.createElement(Pet, {
+            name: 'mbuku',
+            animal: 'rabbit',
+            breed: 'white'
+        }),
+        React.createElement(Pet, {
+            name: 'Babu',
+            animal: 'dog',
+            breed: 'chihuahua'
+        })
+    ]);
+};
 
-        ]
-    );
-}
-
-ReactDOM.render(
-    React.createElement(App, Pet),
-    document.getElementById('root')
-);
+ReactDOM.render(React.createElement(App, Pet), document.getElementById('root'));
